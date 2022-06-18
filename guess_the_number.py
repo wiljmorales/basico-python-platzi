f974c6772e204088b9237e3948868cf147cc520a
Wilfredo Morales <wiljmoralesa@gmail.com>
2022-06-18 13:11:21 -0400

    Adding try counter to the game

--- guess_the_number.py
+++ guess_the_number.py
@@ -3,12 +3,14 @@ def run():
   magic_number = random.randint(1, 100)
   print("Adivina el numero magico")
   user_number = int(input("Elige un numero del 1 al 100: "))
+  count = 1
   while user_number != magic_number:
     if user_number > magic_number:
       user_number = int(input("Elige un numero mas pequeño: "))
-    elif user_number < magic_number:
+    else:
       user_number = int(input("Elige un numero mas grande: "))
+    count += 1
   print("Ganaste! 🥳️, en numero magico es " + str(user_number))
-
+  print("Lo lograste en " + str(count) + " intentos")
 if __name__ == "__main__":
-  run()
+  run() 
